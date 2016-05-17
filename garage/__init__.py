f7cf27c9cb57d0efd44e6c6d08b381b00359429a
gurumitts <miteshcoder1001@gmail.com>
2016-05-16 23:29:07 -0500

    adds webserver

--- garage/__init__.py
+++ garage/__init__.py
@@ -1,6 +1,14 @@
 import logging
 from logging.config import fileConfig
+import web
+from butler import Butler
 
 def start():
     fileConfig('conf/log.conf')
     logging.getLogger('garage').log(logging.DEBUG, 'log setup complete')
+
+    butler = Butler()
+
+    web.start()
+
+
---
+++ garage/web.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import render_template, request, Response
+import math
+from datastore import DataStore
+import json
+import logging
+import os
+
+app = Flask(__name__)
+
+
+
+logs_location = '/var/log/garage'
+
+@app.route('/')
+def index(name=None):
+    db = DataStore()
+    settings = db.get_settings()
+    db.shutdown()
+
+    return render_template('index1.html', )
+
+@app.route('/')
+def hello(name=None):
+    return 'ok'
+
+
+@app.route('/logs/')
+def logs(log=None):
+    logs = []
+    if log is None:
+        for log in os.listdir(logs_location):
+            logs.append(log)
+    return render_template('logs.html', logs=logs)
+
+
+@app.route('/logs/<log>')
+def view_log(log=None):
+    fo = open('%s/%s' %(logs_location, log), 'r')
+    contents = fo.read()
+    return Response(contents, mimetype='text/plain')
+
+def start():
+    # app.debug = True
+    app.run(host='0.0.0.0')
